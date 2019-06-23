81728509f0daa3e27337111da848089083147a84
ASzwarc <szwarc.adam@gmail.com>
2019-06-23 21:21:23 +0200

    First implementation of PyGame's code to draw board for game. Initialization of PyGame. Code needs to be refactored.

--- main.py
+++ main.py
@@ -1,18 +1,50 @@
 import pygame
 import sys
 import Board
+import constants as const
+
+
+def draw_board(board):
+    pygame.draw.rect(
+        screen,
+        const.BLUE,
+        (0, const.MESSAGE_PROMPT_SIZE,
+            const.SCREEN_WIDTH, const.SCREEN_HEIGHT))
+
+    for column in range(const.COLUMN_COUNT):
+        for row in range(const.ROW_COUNT):
+            pygame.draw.circle(
+                screen,
+                const.BLACK,
+                (int(column*const.SQUARE_SIZE + const.SQUARE_SIZE / 2),
+                    int(const.MESSAGE_PROMPT_SIZE +
+                        row*const.SQUARE_SIZE +
+                        const.SQUARE_SIZE/2)),
+                const.RADIUS, 0)
+
+    pygame.display.update()
+
 
 if __name__ == '__main__':
     board = Board.Board()
-    board.print_board()
-    for i in range(3):
-        board.drop_piece(0, i, 2)
-    board.print_board()
-    board.is_move_winning(2)
-    # for i in range(3):
-    #     board.drop_piece(i, 3, 1)
-    board.drop_piece(2, 3, 1)
-    board.drop_piece(1, 2, 1)
-    board.drop_piece(3, 4, 1)
-    board.print_board()
-    board.is_move_winning(1)
+
+    pygame.init()
+    screen_size = (const.SCREEN_WIDTH, const.SCREEN_HEIGHT)
+    screen = pygame.display.set_mode(screen_size)
+
+    myfont = pygame.font.SysFont("monospace", 75)
+
+    draw_board(board.board)
+
+    end_game = False
+
+    while not end_game:
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                end_game = True
+
+        pygame.display.flip()
+
+    # Be IDLE friendly
+    pygame.quit()
