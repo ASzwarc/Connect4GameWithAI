eed3d317369be9b564bc45dbe6f08233afc1bc7b
ASzwarc <szwarc.adam@gmail.com>
2019-06-26 22:56:13 +0200

    New file with different AI classes

---
+++ AI.py
@@ -0,0 +1,24 @@
+"""
+Class containig different oponents AI's
+"""
+
+import random
+from constants import COLUMN_COUNT
+
+
+class AI:
+
+    def __init__(self, board):
+        self._board = board
+
+    def get_next_move(self) -> int:
+        """
+        Returns column in which next piece will be inserted
+        """
+        raise NotImplementedError("This is AI interface method")
+
+
+class RandomAI(AI):
+
+    def get_next_move(self) -> int:
+        return random.randint(0, COLUMN_COUNT - 1)
