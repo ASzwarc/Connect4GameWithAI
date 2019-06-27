b1f6f65309eb3a6852dad12dc4b37270f1c4b07a
ASzwarc <szwarc.adam@gmail.com>
2019-06-28 00:50:13 +0200

    Removed base class and inheritance in RandomAI because it's not needed. Added comments to the class and get_next_move method.

--- AI.py
+++ AI.py
@@ -1,27 +1,23 @@
 """
 File containig different oponents AI classes
 """
-
 import random
 import Board
 from constants import COLUMN_COUNT
 
 
-class AI:
+class RandomAI:
+    """
+    AI that picks next move randomly.
+    """
 
     def __init__(self, board):
         self._board = board
 
     def get_next_move(self) -> int:
         """
-        Returns column in which next piece will be inserted
+        Returns AI's next move
         """
-        raise NotImplementedError("This is AI interface method")
-
-
-class RandomAI(AI):
-
-    def get_next_move(self) -> int:
         column = -1
         while not self._board.is_empty_slot_in(column):
             column = random.randint(0, COLUMN_COUNT - 1)
