871f39cf9d7193a10da9359ee0e93fb2e737df31
ASzwarc <szwarc.adam@gmail.com>
2019-07-04 00:45:45 +0200

    First implementation of minimax algorithm (currently without alpha-beta pruning). Scoring functions was taken from SimpleLogicAI class. Method get_next_move just wraps minimax function and ensures that evaluation is done on temporary deepcopy of board class. Added function which evaluates score after reaching stop condition.

--- AI.py
+++ AI.py
@@ -102,15 +102,98 @@ class MinMaxAI:
     def __init__(self, board):
         self._board = board
 
+    def evaluate_stop_condition(self, board, depth, is_terminal):
+        """
+        Evaluates score after stop condition have been reached.
+        """
+        some_big_score = 1000000
+        if is_terminal:
+            if board.is_move_winning(AI):
+                return (None, some_big_score)
+            elif board.is_move_winning(HUMAN):
+                return (None, -some_big_score)
+            else:  # no more moves
+                return (None, 0)
+        else:  # depth is zero
+            return (None, board.evaluate_window(WINDOW_LENGTH, AI,
+                                                MinMaxAI.evalute_window))
+
+    def minimax(self, board: Board, depth=4: int, maximizingPlayer: bool):
+        """
+        Minimax algorithm loop
+
+        Args:
+            board (Board): Temporary board which will be evaluated
+            depth (int): How many moves algorithm should look-ahead,
+            default is 4
+            maximizingPlayer (bool): If it's player whom score should be maxed
+
+        Returns:
+            Tuple(column (int), score(int)): column with best score
+        """
+        # stop condition
+        is_terminal = board.is_terminal()
+        if depth = 0 or is_terminal:
+            return self.evaluate_stop_condition(board, depth, is_terminal)
+        if maximizingPlayer:  # AI
+            value = -inf
+            best_col = -10
+            columns = board.get_valid_locations()
+            for col in columns:
+                temp_board = copy.deepcopy(board)
+                temp_board.drop_piece_in(col, AI)
+                score = minimax(temp_board, depth - 1, False)[1]
+                if score > value:
+                    value = score
+                    best_col = col
+            return (best_col, value)
+        else:  # player
+            value = inf
+            best_col = -10
+            columns = Board.get_valid_locations()
+            for col in columns:
+                temp_board = copy.deepcopy(board)
+                temp_board.drop_piece_in(col, HUMAN)
+                score = minimax(temp_board, depth - 1, True)[1]
+                if score < value:
+                    value = score
+                    best_col = col
+            return (best_col, value)
+
     def get_next_move(self) -> int:
         """
         Returns AI's next move. Next move is calculated using MinMax algorithm
         """
-        pass
+        temp_board = copy.deepcopy(self._board)
+        result = self.minimax(temp_board, 4, True)
+        print(f"Best result {result[1]} for {result[0]} column")
 
     @staticmethod
     def evaluate_window(self, window, piece) -> int:
         """
         Evaluates score for given window and piece
         """
-        pass
+        if piece == HUMAN:
+            oponent = AI
+        else:
+            oponent = HUMAN
+        score = 0
+        if np.count_nonzero(window == piece) == 4:
+            score += 100
+        elif (np.count_nonzero(window == piece) == 3 and
+                np.count_nonzero(window == 0) == 1):
+            score += 10
+        elif (np.count_nonzero(window == piece) == 2 and
+                np.count_nonzero(window == 0) == 2):
+            score += 5
+        else:
+            pass
+
+        if (np.count_nonzero(window == oponent) == 3 and
+                np.count_nonzero(window == 0) == 1):
+            score -= 80
+
+        if isCenter:
+            score += 6 * np.count_nonzero(window == piece)
+
+        return score
